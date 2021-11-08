00c5fa7fa88b7d9e17f153aed4694d3e3b89cc3b
Marco Vasapollo <ceo@metaring.com>
2021-11-08 14:55:09 +0100

    chore: wip

---
+++ src/lib/utils/truncatedWord.js
@@ -0,0 +1,16 @@
+export default function shortenWord(
+  { context, charsAmount, shortenWordSuffix },
+  word
+) {
+  charsAmount = charsAmount || (context && context.defaultCharsAmount) || 4
+  if (!word) {
+    return ''
+  }
+  var pippo = word.length - 1 - charsAmount
+  if (pippo <= 0) {
+    return word
+  }
+  var firstPart = word.substring(0, charsAmount)
+  var secondPart = word.substring(pippo, word.length - 1)
+  return firstPart + '...' + secondPart
+}
