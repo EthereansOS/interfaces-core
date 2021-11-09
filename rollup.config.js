9227e30a4d4d1eda2b425faab9dcbfba397af80b
Marco Vasapollo <ceo@metaring.com>
2021-11-09 19:49:25 +0100

    chore: wip

--- rollup.config.js
+++ rollup.config.js
@@ -5,22 +5,26 @@ import postcss from 'rollup-plugin-postcss'
 import resolve from '@rollup/plugin-node-resolve'
 import image from '@rollup/plugin-image'
 import visualizer from 'rollup-plugin-visualizer'
-import json from '@rollup/plugin-json';
+import json from '@rollup/plugin-json'
+import sourcemaps from 'rollup-plugin-sourcemaps'
 import pkg from './package.json'
 
 export default {
   input: './src/index.js',
   output: [
     {
+      sourcemap: true,
       file: pkg.main,
       format: 'cjs',
     },
     {
+      sourcemap: true,
       file: pkg.module,
       format: 'esm',
     },
   ],
   plugins: [
+    sourcemaps(),
     external(),
     postcss(),
     babel({
