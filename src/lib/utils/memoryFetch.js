b79d26ad7ff39e773155a29d25f6e85a2fa70598
Marco Vasapollo <ceo@metaring.com>
2022-08-06 13:45:02 +0200

    chore: wip

--- src/lib/utils/memoryFetch.js
+++ src/lib/utils/memoryFetch.js
@@ -1,9 +1,16 @@
 import web3Utils from 'web3-utils'
 
 export default async function memoryFetch(url, type) {
+  var element
+
+  if (url.indexOf('data') === 0) {
+    element = await fetch(url)
+    element = await element[type || 'json']()
+    return element
+  }
+
   var key = web3Utils.sha3(url)
 
-  var element
   try {
     element = JSON.parse(window.localStorage.getItem(key))
   } catch (e) {}
